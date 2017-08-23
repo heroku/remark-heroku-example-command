d39ed00bd6c9c3f0f996e2794da750cfe5a1b341
Matthew Origer <moriger@heroku.com>
2017-08-23 14:18:44 -0500

    fix unhandled promise rejection error

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ async function commandsHaveExamples (ast, file) {
   let headingFound = false
   let exampleFound = false
 
-  const headingOrExample = async (subNode) => {
+  const headingOrExample = async (subNode, index) => {
     if (exampleFound || headingFound) return
-    if (subNode.depth === 3 && subNode !== node) headingFound = true
+    if (subNode.depth === 3 && index !== 0) headingFound = true
     if (subNode.depth === 4 && subNode.children[0].value.includes('Example')) exampleFound = true
   }
 
